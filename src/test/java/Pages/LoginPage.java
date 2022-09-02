edbabbcb7c9fcb53f31c3f5d2b9bced7dd4152cd
Jasdeep-kaur8792 <Jasdeep.kaur@infostride.com>
2022-09-02 00:21:53 -0700

    update

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -10,8 +10,10 @@ public class LoginPage {
     By username = By.xpath("//input[@name='username']");
     By password = By.name("password");
     By submit = By.xpath("//button");
-
-    public LoginPage(WebDriver driver) {
+    By pass= By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[2]/div/div[1]/div/div[2]/input");
+    By empname=By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[2]/div/div[2]/div/div");
+    public LoginPage(WebDriver driver)
+    {
         this.driver = driver;
     }
 
@@ -26,13 +28,15 @@ public class LoginPage {
     public void admin() throws InterruptedException {
         driver.findElement(By.partialLinkText("Admin")).click();
         driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary']")).click();
-//        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/admin/saveSystemUser");
+
     }
 
     public void select() throws InterruptedException {
         driver.findElement(By.xpath("(//div[@class = 'oxd-select-text--after'])[1]")).click();
         driver.findElement(By.xpath("//*[contains(text(),'Admin')]")).click();
-
+        driver.findElement(By.xpath("(//div[@class = 'oxd-select-text--after'])[2]")).click();
+        driver.findElement(By.xpath("//*[contains(text(),'Enabled')]")).click();
+        driver.findElement(pass).sendKeys("Jasdeepk@123");
 
 
     }
